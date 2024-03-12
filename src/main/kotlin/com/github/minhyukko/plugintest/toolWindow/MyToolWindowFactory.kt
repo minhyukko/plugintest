03ab82eee1406c2344ef124b0cb8d59120d03211
GitHub Action <action@github.com>
2024-03-12 14:05:14 +0000

    Template cleanup

--- src/main/kotlin/com/github/minhyukko/plugintest/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/minhyukko/plugintest/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.minhyukko.plugintest.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.minhyukko.plugintest.MyBundle
+import com.github.minhyukko.plugintest.services.MyProjectService
 import javax.swing.JButton
 
 
